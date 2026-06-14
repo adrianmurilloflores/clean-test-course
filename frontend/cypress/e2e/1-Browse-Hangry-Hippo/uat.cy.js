f812d2a1570e833eba0e0af65b69f1d01ce09450
Adrian Murillo <adrianmurilloflores@gmail.com>
2026-06-14 16:50:06 +0300

    smoke tests: Add tests for Handhelds and Appeteasers

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -3,9 +3,17 @@ describe('Shows Appeteasers', () => {
     cy.visit('https://hangryhippo.quantic.host/');
   });
   it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
+  cy.visit('/', { timeout: 120000 }) // Waits up to 2 minutes
+   cy.contains('Handhelds').click();
+   cy.contains('Cheese Burger');
+   cy.contains('Fajita Tacos'); //TODO: Add a test to check Handhelds
   });
   it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+  cy.contains('Appeteasers').click();
+  cy.visit('/', { timeout: 120000 }) // Waits up to 2 minutes
+  cy.contains('Tater Tots');
+  cy.contains('Buffalo Wings');
+  cy.contains('Cheeese Burger').should('not.exist');
+  cy.contains('Fajita Tacos').should('not.exist'); //TODO: Add a test to check Appeteasers
   });
 });
